29c1d365cb1df28081ec63ad566e1a0c2b55ce70
Symion Edwards <symion.edwards@gmail.com>
2020-07-10 10:28:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,6 @@
 def turn_count(board)
   count = 0
+<<<<<<< HEAD
   board.each do |i|
     if i == "X" || i == "O"
       count += 1
@@ -11,3 +12,12 @@ end
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X":"O"
 end
+=======
+  board.each { |element|
+    if element == "X" || element == "0"
+      count += 1
+    }
+  end
+  return count
+end
+>>>>>>> 9231a799509175aafe946b14c9a72f1f4b3cec13
